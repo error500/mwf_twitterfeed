f604f0d8d414e974d70da3e413f2ade601f97f94
error500 <error500@free.fr>
2018-02-24 11:19:40 +0100

    fix when no tweets display nothing

--- class/MwfTwitterFeed.php
+++ class/MwfTwitterFeed.php
@@ -65,66 +65,70 @@ public function __construct()
 
         $this->tweets = $this->readTweets();
 
+        if ($this->tweets) {
 
-		ob_start();		
-		?>
-			<div id="tweets">
-				<a class="follow-button right" title="Suivre <?php echo $this->twitterName; ?> sur Twitter"  role="button" href="https://twitter.com/<?php echo $this->twitterAccount;?>">
-					<i class="ic-button-bird"></i>Suivre
-				</a>
-				<h2>Tweets</h2>
+            ob_start();
+            ?>
+            <div id="tweets">
+                <a class="follow-button right" title="Suivre <?php echo $this->twitterName; ?> sur Twitter"  role="button" href="https://twitter.com/<?php echo $this->twitterAccount;?>">
+                    <i class="ic-button-bird"></i>Suivre
+                </a>
+                <h2>Tweets</h2>
+                <ol>
+                    <?php foreach ($this->tweets as $tweet){
+                        ?>
+                        <li class="tweet">
+                            <div class="header">
+                                <?php
+                                if(isset($tweet->retweeted_status)) {
+                                    ?>
+                                    <div class="h-card p-author" data-scribe="component:author">
+                                        <a href="<?php echo $tweet->retweeted_status->user->url?>">
+                                            <strong class="fullname"><?php echo $tweet->retweeted_status->user->name?></strong>
+                                            <span class="p-nickname">@<?php echo $tweet->retweeted_status->user->screen_name?></span>
+                                            <img class="avatar" src="<?php
+                                            echo $this->checkHttps() ? $tweet->retweeted_status->user->profile_image_url_https : $tweet->retweeted_status->user->profile_image_url;
+                                            ?>" />
+                                        </a>
+                                    </div>
+                                    <p  class="e-entry-title" ><?php echo preg_replace('/((www|http:\/\/|https:\/\/)[^ ]+)/', '<a href="\1">\1</a>', $tweet->retweeted_status->text); ?></p>
+                                    <div class="retweet-credit" class="right"><i class="ic-rt"></i>Retweet&eacute; par <?php echo $this->twitterName; ?></div>
+                                    <?php
+                                } else {
+                                    ?>
+                                    <div class="h-card p-author" data-scribe="component:author">
+                                        <a href="<?php echo $tweet->user->url?>">
+                                            <strong class="fullname"><?php echo $tweet->user->name?></strong>
+                                            <span class="p-nickname">@<?php echo $tweet->user->screen_name?></span>
+                                            <img class="avatar" src="<?php
+                                            echo $this->checkHttps() ? $tweet->user->profile_image_url : $tweet->user->profile_image_url?>" />
+                                        </a>
+                                    </div>
+                                    <p class="e-entry-title" ><?php echo preg_replace('/((www|http:\/\/|https:\/\/)[^ ]+)/', '<a href="\1">\1</a>', $tweet->text); ?></p>
+                                    <?php
+                                } ?>
 
-				<ol>
-				<?php foreach ($this->tweets as $tweet){
-					?>
-					<li class="tweet">
-						<div class="header">
+                                <?php if (isset($tweet->entities->media)) {
+                                    foreach ($tweet->entities->media as $media) {
+                                        if ( $media->type == "photo" ) { ?>
+                                            <img src="<?php echo  $this->checkHttps() ? $media->media_url_https : $media->media_url ?>" />
+                                        <?php }
+                                    }
+                                }?>
+                            </div>
+                        </li>
+                    <?php } //endforeach ?>
+                </ol>
+            </div>
+            <?php
 
-							<?php 
-							if(isset($tweet->retweeted_status)) {
-							?>
-								<div class="h-card p-author" data-scribe="component:author">
-									<a href="<?php echo $tweet->retweeted_status->user->url?>">
-									<strong class="fullname"><?php echo $tweet->retweeted_status->user->name?></strong>
-									<span class="p-nickname">@<?php echo $tweet->retweeted_status->user->screen_name?></span>
-									<img class="avatar" src="<?php
-                                    echo $this->checkHttps() ? $tweet->retweeted_status->user->profile_image_url_https : $tweet->retweeted_status->user->profile_image_url;
-                                    ?>" />
-									</a>
-								</div>
-								<p  class="e-entry-title" ><?php echo preg_replace('/((www|http:\/\/|https:\/\/)[^ ]+)/', '<a href="\1">\1</a>', $tweet->retweeted_status->text); ?></p>
-								<div class="retweet-credit" class="right"><i class="ic-rt"></i>Retweet&eacute; par <?php echo $this->twitterName; ?></div>
-							<?php 
-							} else {
-							?>
-								<div class="h-card p-author" data-scribe="component:author">
-									<a href="<?php echo $tweet->user->url?>">
-										<strong class="fullname"><?php echo $tweet->user->name?></strong>
-										<span class="p-nickname">@<?php echo $tweet->user->screen_name?></span>
-										<img class="avatar" src="<?php
-                                        echo $this->checkHttps() ? $tweet->user->profile_image_url : $tweet->user->profile_image_url?>" />
-									</a>
-								</div>
-								<p class="e-entry-title" ><?php echo preg_replace('/((www|http:\/\/|https:\/\/)[^ ]+)/', '<a href="\1">\1</a>', $tweet->text); ?></p>
-							<?php 
-							} ?>
+            $render = ob_get_contents();
 
-							<?php if (isset($tweet->entities->media)) {
-								foreach ($tweet->entities->media as $media) {
-									if ( $media->type == "photo" ) { ?>
-										<img src="<?php echo  $this->checkHttps() ? $media->media_url_https : $media->media_url ?>" />
-									<?php }
-								}
-							}?>
-						</div>
-					</li>
-				<?php } //endforeach ?>
-				</ol>
+            ob_end_clean();
 
-			</div>
-		<?php
-		$render = ob_get_contents();
-		ob_end_clean();
+        }
+
+
 			
 		return $render;
 
